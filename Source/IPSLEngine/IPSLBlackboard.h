579c28d02769dc28c0eab093cd33047ccdd2c216
Arne Hasselbring <arne.hasselbring@tu-harburg.de>
2018-01-06 16:49:48 +0100

    Add IPSLBlackboard class

---
+++ Source/IPSLEngine/IPSLBlackboard.h
@@ -0,0 +1,34 @@
+/**
+ * @file IPSLBlackboard.h
+ *
+ * This file declares and implements the IPSLBlackboard class.
+ *
+ * @author Arne Hasselbring
+ */
+
+#pragma once
+
+#include <string>
+
+#include "IPSLToken.h"
+
+/**
+ * @brief This class stores the data that are relevant to the IPSL execution process.
+ */
+class IPSLBlackboard final
+{
+public:
+  /**
+   * @brief Constructs a blackboard.
+   * @param inputName The name of the input stream.
+   * @param source The source code.
+   */
+  IPSLBlackboard(const std::string& inputName, const std::string& source) :
+    inputName(inputName),
+    source(source)
+  {
+  }
+  std::string inputName;                     ///< The name of the input stream.
+  std::string source;                        ///< The source code.
+  IPSLTokenSequence tokenSequence;           ///< The token sequence (only valid after lexical analysis).
+};
